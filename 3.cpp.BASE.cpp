4faf519ba494b836ef7cc2086df639234f45dc66
tomaszpochron <tomasz.pochron29@gmail.com>
2016-11-03 13:43:52 +0100

    konflikt

---
+++ 3.cpp.BASE.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <math.h>
+#include <windows.h>
+using namespace std;
+
+int main()
+{
+    int liczba1, liczba2, pierwiastek1, pierwiastek2;
+    cout << "Podaj pierwsza liczbe: ";
+    cin >> liczba1;
+    cout << "Podaj druga liczbe:";
+    cin >> liczba2;
+     pierwiastek1 = sqrt (liczba1);
+cout << "Dodawanie: "
+     << liczba1 + liczba2 << endl;
+cout << "Odejmowanie: "
+     << liczba1 - liczba2 << endl;
+cout << "Mnozenie: "
+     << liczba1 + liczba2 << endl;
+cout << "Dzielenie: "
+
+
+
+     << liczba1 / liczba2 << endl;
+cout << "Reszta z dzielenia:"
+     << liczba1 % liczba2 << endl;
+cout << "Pierwiastek z liczby1" << endl;
+     << pierwiastek1 << endl;
+
+Sleep(10000);
+}
