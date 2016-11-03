d32fba4c3b607f29558ac8ae0c060d7937af94ab
tomaszpochron <tomasz.pochron29@gmail.com>
2016-11-03 13:54:25 +0100

    2

--- 2.cpp
+++ 2.cpp
@@ -1,17 +1,20 @@
+
+//2
+
 #include <iostream>
 #include <windows.h>
   #include <conio.h>
-     
+
 using namespace std;
- 
+
 double pobierz(int liczba){
        while(!(cin>>liczba)){
             cout << "Podaj poprawna wartosc: ";
-  cin.clear(); 
+  cin.clear();
   cin.sync();}
   return liczba;
        }
- 
+
 int main() {
       start:
     system("title SoundSkan");
@@ -20,31 +23,31 @@ int main() {
   cout << "--------------------------------------------------------------------------" << endl;
     int min, max;
     int interwal;
-   
+
     cout << "Podaj czestotliwosc minimalna: ";
   min = pobierz(min);
   cout << "Podaj czestotliowsc maksymalna: ";
   max = pobierz(max);
   cout << "Podaj interwal(odstep): ";
   interwal = pobierz(interwal);
-   
+
 system("color 02");
     for(int a=min; a<max; a= a+interwal)
     {
-    
+
              cout<< "Aktualna czestotliwosc: "<< a << endl;
               Beep(a,200);
               system("cls");
              }
-     
-     
+
+
     cout << "Wytrzymales? Zakonczyc? T/N";
     char znak;
-    znak = getch();  
+    znak = getch();
     if(znak != 't')
     {
     system("cls");
-        goto start;  
+        goto start;
             }
- return 0;   
+ return 0;
 }
